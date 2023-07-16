2abc6db8760abd498c0acc12ba84a3f3ed822bd8
chumakov <igor2011__igor@mail.ru>
2023-07-16 13:27:01 +0400

    little bit change on works view

--- resources/views/works.blade.php
+++ resources/views/works.blade.php
@@ -3,14 +3,21 @@
 @section('content')
     
     @foreach($works as $work)
-        <div class="work" data-id="{{ $work->id }}">
+        @php 
+            $addClass = (!empty($work->url_files))?' work_have_preview':'';
+        @endphp
+        <div class="work {{ $addClass }}" data-id="{{ $work->id }}">
             <!-- <div class="border"></div> -->
             <div class="content">
                 <h3>{{  $work->title  }}</h3>
                 <p>{{  $work->description  }}</p>
                 <a href="{{ $work->url_work }}">{{ $work->url_work }}</a>
                 <!-- <h3>{{  $work->title  }}</h3> -->
-                {{  $work->url_files }}
+                @if (!empty($work->url_files))
+                    @foreach(explode(',', $work->url_files) as $filesPath)
+                        <img src="{{ $filesPath }}" alt="" class="work_preview">
+                    @endforeach
+                @endif
             </div>
             <div class="work_actions">
                 <div class="work_action custom-btn clr-danger">
