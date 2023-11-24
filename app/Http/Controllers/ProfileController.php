f3992bf99ebd49e3029b66edc849e1b011fdb8b5
chumakov <igor2011__igor@mail.ru>
2023-11-25 00:27:20 +0400

    fix upd img, res data

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -50,12 +50,8 @@ class ProfileController extends Controller
             $photo = $request->file('user_avatar');
             $photoExt = $photo->extension();
             
-            if (Helpers::$acceptFileSize < $photo->size){
-                return Helpers::jsonRespose(
-                    [
-                        'success' => false,
-                        'response' => 'Файл превышает 3МБ'
-                    ]);
+            if (Helpers::$acceptFileSize < $photo->getSize()){
+                return Helpers::jsonRespose(false,'Файл превышает 3МБ');
             }
             
             $photoNewName = Hash::make('user_'.$userId.'_avatar', ['rounds' => 5]);
@@ -70,7 +66,6 @@ class ProfileController extends Controller
             $workerFind = Workers::query()
                 ->where('user_id', '=', $userId)
                 ->where('url_avatar', '<>', null)
-                ->select('url_avatar')
                 ->first();
 
             $data['old_url_avatar'] = $workerFind->url_avatar;
@@ -83,18 +78,21 @@ class ProfileController extends Controller
             }
 
             $workerFind->url_avatar = $data['url_avatar'];
-            
-            $res = $workerFind->save() ? [ 'success' => true, 'result' => 'Аватар успешно изменен' ]
-                : [ 'success' => false, 'errors' => 'Не удалось изменить аватарку' ];
+
+            $res = $workerFind->save();
+            $res = $res ? [ 'success' => true, 'res' => 'Аватар успешно изменен' ]
+                : [ 'success' => false, 'res' => ['error' => 'Не удалось изменить аватарку'] ];
             
         } else {
             $res = [
                 'success' => false,
-                'error' => 'Не удалось задать новую аватарку'
+                'res' => [
+                    'error' => 'Не удалось задать новую аватарку'
+                ]
             ];
         }
 
-        return Helpers::jsonRespose($res);
+        return Helpers::jsonRespose($res['success'], $res['res']);
     }
     
 }
