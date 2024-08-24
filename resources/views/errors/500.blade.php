48b60d5927389c0842031f27959d97d72ade0b23
chokoladis <igor2011__igor@mail.ru>
2024-08-24 21:52:02 +0400

    add 500 page, fix gitignore

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main')
+
+@php
+    $arImg = [
+        '/storage/general/errors/500_1.gif',
+        '/storage/general/errors/500_2.png',
+    ];
+
+    $randomImg = $arImg[rand(0,1)];
+@endphp
+
+@section('content')
+    <main>
+        <div class="container">
+            <div class="uk-card text-center mt-5">
+                <h3 class="uk-text-warning">{{ __('Не правильно #bанные волки, широкую на широкую') }}</h3>
+                <p class="uk-text-warning">{{ __('На сервере что то отработало не правильно') }}</p>
+                <h1>500</h1>
+                <img src="{{ $randomImg }}" alt="">
+            </div>
+        </div>
+    </main>
+@endsection
